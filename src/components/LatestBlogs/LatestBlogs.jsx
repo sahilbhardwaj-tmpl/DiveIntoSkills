363fac1d98de9382ebce84f83a502ccde7e9bfaa
sahil@tunica <sahil.b@tunica.tech>
2024-01-18 10:06:53 +0530

    Added latest blogs through api but categories are left also pagination is added

--- src/components/LatestBlogs/LatestBlogs.jsx
+++ src/components/LatestBlogs/LatestBlogs.jsx
@@ -1,82 +1,93 @@
-import React from 'react'
-import './LatestBlogStyles.css'
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import NewsLetterPopUp from '../NewsLetterPopUp/NewsLetterPopUp'
-import CardData from '../Card/CardData'
-import CardBuilder from '../Card/CardBuilder'
+import React from "react";
+import "./LatestBlogStyles.css";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import NewsLetterPopUp from "../NewsLetterPopUp/NewsLetterPopUp";
+import CardData from "../Card/CardData";
+import CardBuilder from "../Card/CardBuilder";
+import axios from "axios";
 function LatestBlogs() {
-    const itemsPerPage = 3;
-    const [displayedData, setDisplayedData] = useState(CardData.slice(0, itemsPerPage));
-
+    const [pageNumber, setPageNumber] = useState(1);
+    const itemsPerPage = 10;
+    const [blogs, setBlogs] = useState([]);
+    const [hasMoreBlogs, setHasMoreBlogs] = useState(true);
+  
+    useEffect(() => {
+        fetchBlogs();
+    }, [pageNumber]);
+  
+    const fetchBlogs = () => {
+      axios
+        .get(`https://diveintoskill.onrender.com/blogs?pageno=${pageNumber}&pagesize=${itemsPerPage}`)
+        .then((response) => {
+            console.log(response?.data,"here");
+          if (response.data.length > 0) {
+            setBlogs((prevBlogs) => [...prevBlogs, ...response.data]);
+            setHasMoreBlogs(true);
+          } else {
+            setHasMoreBlogs(false);
+          }
+        })
+        .catch((error) => {
+          console.error("Error fetching blogs:", error);
+        });
+    };
+  
     const handleLoadMore = () => {
-        const currentLength = displayedData.length;
-        const newData = CardData.slice(currentLength, currentLength + itemsPerPage);
-        setDisplayedData([...displayedData, ...newData]);
-    }
-
+        setPageNumber((prevPageNumber) => prevPageNumber + 1);
+    };
+  
     return (
-        <>
-            <div className="blog-page">
-                <h1 style={{ textAlign: 'center', marginTop: '20px' }}>Latest Blogs</h1>
-
-                <div className="blog-layout">
-
-                    <div className="category-section">
-                        <h2 className='category-heading'>Categories</h2>
-                        <div className='categories'>
-                            <ul className='category-links'>
-                                <li><Link ><p style={{ fontSize: '20px' }}>Category 1</p></Link></li>
-                                <li><Link ><p style={{ fontSize: '20px' }}>Category 2</p></Link></li>
-                                <li><Link ><p style={{ fontSize: '20px' }}>Category 3</p></Link></li>
-                                <li><Link ><p style={{ fontSize: '20px' }}>Category 4</p></Link></li>
-                                <li><Link ><p style={{ fontSize: '20px' }}>Category 5</p></Link></li>
-                                <li><Link ><p style={{ fontSize: '20px' }}>Category 6</p></Link></li>
-                                
-                            </ul>
-                            
-                            
-                           
-                        </div>
-
-
-                    </div>
-
-
-                    <div className="blogs-section">
-
-                        <div className='cards'>
-                            {displayedData.map((data, indx) => {
-                                return (
-                                    <Link className='card-hover' to="/SinglePost">
-                                        <CardBuilder key={indx}
-                                        img={data.blogImg}
-                                        title={data.title}
-                                        description={data.description}
-                                        date={data.date}
-                                        author={data.author} />
-                                    </Link>
-                                    
-                                )
-                            })}
-                        </div>
-                        <div className='btn-container'>
-                            {CardData.length > displayedData.length && (
-                                <button className='btn-styles' style={{
-                                    cursor: 'pointer',
-                                    color: '#FCEE21',
-                                    textAlign: 'center',
-                                    fontSize: '20px'
-                                }} onClick={handleLoadMore}>Load More</button>
-                            )}
-                        </div>
-
-                    </div>
-                </div>
+      <>
+        <div className="blog-page">
+          <h1 style={{ textAlign: "center", marginTop: "20px" }}>Latest Blogs</h1>
+  
+          <div className="blog-layout">
+            <div className="category-section">
+              <h2 className="category-heading">Categories</h2>
+              <div className="categories">
+                <ul className="category-links">
+                 
+                </ul>
+              </div>
             </div>
-            <NewsLetterPopUp />
-        </>
-    )
-}
+  
+            <div className="blogs-section">
+              <div className="cards">
+                {blogs.map((data) => (
+                  <Link key={data.id} className="card-hover" to= {`/SinglePost/${data.id}`}>
+                    <CardBuilder
+                      img={data.link}
+                      title={data.title}
+                      description={data.description}
+                      date={data.createdAt}
+                      author={data.author.name}
+                    />
+                  </Link>
+                ))}
+              </div>
+              <div className="btn-container">
+                {hasMoreBlogs && (
+                  <button
+                    className="btn-styles"
+                    style={{
+                      cursor: 'pointer',
+                      color: '#FCEE21',
+                      textAlign: 'center',
+                      fontSize: '20px',
+                    }}
+                    onClick={handleLoadMore}
+                  >
+                    Load More
+                  </button>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+        <NewsLetterPopUp />
+      </>
+    );
+  }
 
-export default LatestBlogs
+export default LatestBlogs;
