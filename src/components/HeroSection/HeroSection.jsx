e208cad43b80d663640433eb989785b2198c49e5
sahil@tunica <sahil.b@tunica.tech>
2024-01-18 13:29:23 +0530

    Fixed footer UI in accordance to figma

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -6,9 +6,9 @@ function HeroSection() {
         <>
             <div className='hero-container'>
                 <div className='textContainer'>
-                    <h1 style={{color:'#FCEE21'}}>The latest articles and courses to help you upgrade your skills.</h1>
+                    <h1 className='h1'style={{color:'#FCEE21'}}>The latest articles and courses to help you upgrade your skills.</h1>
 
-                    <p style={{color:'#FFF',marginTop:'25px'}}>Master Web Development with amazing resources that are available for free! Join our Newsletter and get alerted when new articles, topics or courses are published.</p>
+                    <p className='p'style={{color:'#FFF',marginTop:'25px'}}>Master Web Development with amazing resources that are available for free! Join our Newsletter and get alerted when new articles, topics or courses are published.</p>
                 </div>
                 <div className='image-container'>
                     <img src={vectorImage} alt='Vector Image' />
