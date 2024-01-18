6bc1f44d1937694e61c2e11b8c636d1da05d6e4a
sahil@tunica <sahil.b@tunica.tech>
2024-01-18 13:52:39 +0530

    Fixed UI of footer in accordanc to figma

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -15,7 +15,7 @@ function Footer() {
         </div>
         <div className="text-container">
           <div className="txt">
-            <p style={{ color: "#DCDE9F", fontSize: "20px" }}>
+            <p className='txt-p' style={{  }}>
               The latest articles and courses to help you upgrade your skills
               and learn the latest trends. Master web Development with these
               amazing resources that are available for free!
