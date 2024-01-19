5a24ccd424fb6657c6fd02e2891a8d9ab89c4042
sahil@tunica <sahil.b@tunica.tech>
2024-01-19 10:39:07 +0530

    Added loader in latest blog page and fixed load more button for mobile view

--- src/components/LatestBlogs/LatestBlogs.jsx
+++ src/components/LatestBlogs/LatestBlogs.jsx
@@ -3,98 +3,114 @@ import "./LatestBlogStyles.css";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import NewsLetterPopUp from "../NewsLetterPopUp/NewsLetterPopUp";
-import CardData from "../Card/CardData";
+import { ClipLoader } from "react-spinners";
 import CardBuilder from "../Card/CardBuilder";
 import axios from "axios";
 function LatestBlogs() {
-    const [pageNumber, setPageNumber] = useState(1);
-    const itemsPerPage = 9;
-    const [blogs, setBlogs] = useState([]);
-    const [hasMoreBlogs, setHasMoreBlogs] = useState(true);
-  
-    useEffect(() => {
-        fetchBlogs();
-    }, [pageNumber]);
-  
-    const fetchBlogs = () => {
-      axios
-        .get(`https://diveintoskill.onrender.com/blogs?pageno=${pageNumber}&pagesize=${itemsPerPage}`)
-        .then((response) => {
-            console.log(response?.data,"here");
-          if (response.data.length > 0) {
-            setBlogs((prevBlogs) => [...prevBlogs, ...response.data]);
-            setHasMoreBlogs(true);
-          } else {
-            setHasMoreBlogs(false);
-          }
-        })
-        .catch((error) => {
-          console.error("Error fetching blogs:", error);
-        });
-    };
-  
-    const handleLoadMore = () => {
-        setPageNumber((prevPageNumber) => prevPageNumber + 1);
-    };
-  
-    return (
-      <>
-        <div className="blog-page">
-          <p className="latest-blog-heading"style={{ }}>Latest Blogs</p>
-  
-          <div className="blog-layout">
-            <div className="category-section">
-              <p className="category-heading">Categories</p>
-              <div className="categories">
-                <ul className="category-links">
-                  <li><p className="each-category">Category 1</p></li>
-                  <li><p className="each-category">Category 1</p></li>
-                  <li> <p className="each-category">Category 1</p></li>
-                  <li><p className="each-category">Category 1</p></li>
-                  <li><p className="each-category">Category 1</p></li>
-                  <li><p className="each-category">Category 1</p></li>
-   
-                </ul>
-              </div>
-            </div>
-  
-            <div className="blogs-section">
-              <div className="cards">
-                {blogs.map((data) => (
-                  <Link key={data.id} className="card-hover" to= {`/SinglePost/${data.id}`}>
-                    <CardBuilder
-                      img={data.link}
-                      title={data.title}
-                      description={data.description}
-                      date={data.createdAt}
-                      author={data.author.name}
-                    />
-                  </Link>
-                ))}
-              </div>
-              <div className="btn-container">
-                {hasMoreBlogs && (
-                  <button
-                    className="btn-styles"
-                    style={{
-                      cursor: 'pointer',
-                      color: '#FCEE21',
-                      textAlign: 'center',
-                      fontSize: '30px',
-                      
-                    }}
-                    onClick={handleLoadMore}
-                  >
-                    Load More
-                  </button>
-                )}
-              </div>
+  const [pageNumber, setPageNumber] = useState(1);
+  const itemsPerPage = 9;
+  const [blogs, setBlogs] = useState([]);
+  const [hasMoreBlogs, setHasMoreBlogs] = useState(true);
+
+  useEffect(() => {
+    fetchBlogs();
+  }, [pageNumber]);
+
+  const fetchBlogs = () => {
+    axios
+      .get(
+        `https://diveintoskill.onrender.com/blogs?pageno=${pageNumber}&pagesize=${itemsPerPage}`
+      )
+      .then((response) => {
+        console.log(response?.data, "here");
+        if (response.data.length > 0) {
+          setBlogs((prevBlogs) => [...prevBlogs, ...response.data]);
+          setHasMoreBlogs(true);
+        } else {
+          setHasMoreBlogs(false);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching blogs:", error);
+      });
+  };
+
+  const handleLoadMore = () => {
+    setPageNumber((prevPageNumber) => prevPageNumber + 1);
+  };
+
+  return (
+    <>
+      <div className="blog-page">
+        <p className="latest-blog-heading" style={{}}>
+          Latest Blogs
+        </p>
+
+        <div className="blog-layout">
+          <div className="category-section">
+            <p className="category-heading">Categories</p>
+            <div className="categories">
+              <ul className="category-links">
+                <li>
+                  <p className="each-category">Category 1</p>
+                </li>
+                <li>
+                  <p className="each-category">Category 1</p>
+                </li>
+                <li>
+                  {" "}
+                  <p className="each-category">Category 1</p>
+                </li>
+                <li>
+                  <p className="each-category">Category 1</p>
+                </li>
+                <li>
+                  <p className="each-category">Category 1</p>
+                </li>
+                <li>
+                  <p className="each-category">Category 1</p>
+                </li>
+              </ul>
             </div>
           </div>
+
+          <div className="blogs-section">
+            {blogs.length === 0 ? (
+              <ClipLoader color="#183114" loading={true} size={60} />
+            ) : (
+              <>
+                <div className="cards">
+                  {blogs.map((data) => (
+                    <Link
+                      key={data.id}
+                      className="card-hover"
+                      to={`/SinglePost/${data.id}`}
+                    >
+                      <CardBuilder
+                        img={data.link}
+                        title={data.title}
+                        description={data.description}
+                        date={data.createdAt}
+                        author={data.author.name}
+                      />
+                    </Link>
+                  ))}
+                </div>
+                <div className="btn-container">
+                  {hasMoreBlogs && (
+                    <button className="boton" onClick={handleLoadMore}>
+                      Load More
+                    </button>
+                  )}
+                </div>
+              </>
+            )}
+          </div>
         </div>
-        <NewsLetterPopUp />
-      </>
-    );
-  }
+      </div>
+      <NewsLetterPopUp />
+    </>
+  );
+}
 
 export default LatestBlogs;
